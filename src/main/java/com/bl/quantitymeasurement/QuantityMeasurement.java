d9910400c44ba120c7204e328955eb299f234b6e
sid-thakur47 <sidthakur258@gmail.com>
2020-05-16 15:53:18 +0530

    ADDED:Functionality to check for null

--- src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
@@ -1,4 +1,17 @@
 package com.bl.quantitymeasurement;
 
 public class QuantityMeasurement {
+    int feet;
+
+    public QuantityMeasurement(int feet) {
+        this.feet = feet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        return true;
+    }
 }
