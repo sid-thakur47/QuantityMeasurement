dbb81d00858d45ed2cd6084b0989bf13651e77e9
sid-thakur47 <sidthakur258@gmail.com>
2020-05-16 15:57:18 +0530

    ADDED:Test Case for null check

--- src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
@@ -1,4 +1,13 @@
 package com.bl.quantitymeasurement;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class QuantityMeasurementTest {
+    @Test
+    public void givenZeroFeet_When_ZeroFeet_ShouldReturnZero() {
+        QuantityMeasurement firstMeasurement=new QuantityMeasurement(0);
+        QuantityMeasurement secondMeasurement=new QuantityMeasurement(0);
+        Assert.assertEquals( firstMeasurement,secondMeasurement );
+    }
 }
