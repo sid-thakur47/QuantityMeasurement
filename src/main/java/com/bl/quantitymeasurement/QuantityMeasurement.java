7fb7a65e49543827b8d15399f6963904564e89d1
sid-thakur47 <sidthakur258@gmail.com>
2020-05-17 11:42:32 +0530

    ADDED:Test and code to check for Value equality

--- src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
@@ -12,7 +12,8 @@ public class QuantityMeasurement {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o != null) return true;
-        else return false;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuantityMeasurement that = (QuantityMeasurement) o;
+        return feet == that.feet;
     }
 }
--- src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
@@ -4,22 +4,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
-    QuantityMeasurement quantityMeasurement=new QuantityMeasurement();
+    QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
 
     @Test
     public void givenZeroFeet_When_ZeroFeet_ShouldReturnZero() {
-        QuantityMeasurement firstMeasurement=new QuantityMeasurement(0);
-        QuantityMeasurement secondMeasurement=new QuantityMeasurement(0);
-        Assert.assertEquals( firstMeasurement,secondMeasurement );
+        QuantityMeasurement firstMeasurement = new QuantityMeasurement( 0 );
+        QuantityMeasurement secondMeasurement = new QuantityMeasurement( 0 );
+        Assert.assertEquals( firstMeasurement, secondMeasurement );
     }
+
     @Test
     public void givenFeet_When_Null_ShouldReturnFalse() {
         boolean checkNull = quantityMeasurement.equals( null );
-        Assert.assertEquals( false,checkNull );
+        Assert.assertEquals( false, checkNull );
     }
+
     @Test
     public void givenFeet_When_SameReference_ShouldReturnTrue() {
-        QuantityMeasurement reference=new QuantityMeasurement();
-        Assert.assertEquals( reference,quantityMeasurement );
+        QuantityMeasurement reference = new QuantityMeasurement();
+        Assert.assertEquals( reference, quantityMeasurement );
+    }
+
+    @Test
+    public void givenFeet_When_SameValue_ShouldReturnTrue() {
+        QuantityMeasurement firstMeasurement = new QuantityMeasurement( 10 );
+        QuantityMeasurement secondMeasurement = new QuantityMeasurement( 10 );
+        Assert.assertEquals( firstMeasurement, secondMeasurement );
     }
 }
