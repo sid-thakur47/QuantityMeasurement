a535d25568e91cd32764cd9139fe71ec29256688
sid-thakur47 <sidthakur258@gmail.com>
2020-05-17 11:09:59 +0530

    ADDED:Test code for reference equality

--- src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
@@ -3,15 +3,16 @@ package com.bl.quantitymeasurement;
 public class QuantityMeasurement {
     int feet;
 
+    public QuantityMeasurement() {
+    }
+
     public QuantityMeasurement(int feet) {
         this.feet = feet;
     }
-
     @Override
     public boolean equals(Object o) {
-        if (o == null) {
-            return false;
-        }
-        return true;
+        if (this == o) return true;
+        if (o != null) return true;
+        else return false;
     }
 }
