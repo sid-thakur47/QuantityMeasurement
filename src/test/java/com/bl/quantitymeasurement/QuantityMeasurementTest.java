e2fded5c52c019fa37297a2e76a22456586aad4b
sid-thakur47 <sidthakur258@gmail.com>
2020-05-17 11:10:25 +0530

    ADDED:Test Case to check for Reference equality

--- src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
@@ -4,10 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
+    QuantityMeasurement quantityMeasurement=new QuantityMeasurement();
+
     @Test
     public void givenZeroFeet_When_ZeroFeet_ShouldReturnZero() {
         QuantityMeasurement firstMeasurement=new QuantityMeasurement(0);
         QuantityMeasurement secondMeasurement=new QuantityMeasurement(0);
         Assert.assertEquals( firstMeasurement,secondMeasurement );
+    }
+    @Test
+    public void givenFeet_When_Null_ShouldReturnFalse() {
+        boolean checkNull = quantityMeasurement.equals( null );
+        Assert.assertEquals( false,checkNull );
+    }
+    @Test
+    public void givenFeet_When_SameReference_ShouldReturnTrue() {
+        QuantityMeasurement reference=new QuantityMeasurement();
+        Assert.assertEquals( reference,quantityMeasurement );
     }
 }
