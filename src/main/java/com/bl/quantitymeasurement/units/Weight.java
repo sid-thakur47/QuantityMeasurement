fb8938018e942bb9dd6e8362c96db7822fe1fe6a
sid-thakur47 <sidthakur258@gmail.com>
2020-05-19 17:15:25 +0530

    ADDED:Functionality for comparing Kg and Grams

---
+++ src/main/java/com/bl/quantitymeasurement/units/Weight.java
@@ -0,0 +1,22 @@
+package com.bl.quantitymeasurement.units;
+
+import com.bl.quantitymeasurement.QuantityMeasurement;
+import com.bl.quantitymeasurement.constant.QuantityMeasurementConstant;
+
+public class Weight extends QuantityMeasurement implements QuantityMeasurementConstant {
+
+    public Weight(Unit unit, double value) {
+        super( unit, value );
+    }
+
+    public Weight() {
+    }
+
+    public boolean compareUnits(Weight measurement) {
+
+        if (this.unit.equals( Unit.KILO_GRAM ) && (measurement.unit.equals( Unit.GRAMS ))) {
+            return this.value * KILOGRAM_GRAM == measurement.value;
+        }
+        return false;
+    }
+}
